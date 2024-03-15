b2ca161193c21a7f0fa53d6ffaecfbbe46ea217c
JuanAlvarezzzDev <juanalvarezzz.dev@gmail.com>
2024-03-15 01:37:39 -0500

    create function applyfilters

--- client/src/views/Inicio.jsx
+++ client/src/views/Inicio.jsx
@@ -2,25 +2,37 @@ import useSWR from "swr";
 import Producto from "../components/Producto";
 import clienteAxios from "../config/axios";
 import useQuisco from "../hooks/useQuiosco";
+import { useEffect, useState } from "react";
 
 export default function Inicio() {
-  const { categoriaActual, listProductos, busqueda } = useQuisco();
+  const { categoriaActual, listProductos, busqueda, handleClickCategoria } = useQuisco();
+  const [filterProduct, setFilterProduct] = useState([]);
+  const applyFilters = () => {
+    let productos = listProductos;
 
-  
-  let productos = listProductos.filter(
-    (producto) => producto.categoria_id === categoriaActual.id
-  );
-
-  if (busqueda.trim() !== "") {
-    productos = listProductos.filter((producto) =>
+    if (categoriaActual.id > 0) {
+      productos = listProductos.filter(
+        (producto) => producto.categoria_id === categoriaActual.id
+      );
+    }
+    if (busqueda) {
+      productos = listProductos.filter((producto) =>
       producto.nombre.toLowerCase().includes(busqueda.toLowerCase())
     );
-  }
+    }
+
+    setFilterProduct(productos);
+  };
+
+  useEffect(() => {
+    applyFilters();
+  }, [categoriaActual, busqueda, handleClickCategoria]);
+ 
 
   return (
     <>
-      <div className="grid gap-4 grid-cols-1 md:grid-cols-2 xl:grid-cols-3">
-        {productos.map((producto) => (
+      <div className="grid gap-4 grid-cols-2 md:grid-cols-2 xl:grid-cols-3">
+        {filterProduct.map((producto) => (
           <Producto
             key={producto.imagen}
             producto={producto}
