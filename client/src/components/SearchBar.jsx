d247fee95179218430f5733aa1deabf4bb82a423
JuanAlvarezzzDev <juanalvarezzz.dev@gmail.com>
2024-03-15 00:28:19 -0500

    edit style searchBar

--- client/src/components/SearchBar.jsx
+++ client/src/components/SearchBar.jsx
@@ -3,12 +3,12 @@ import { FaSearch } from "react-icons/fa";
 
 const SearchBar = ({ value, changeInput }) => {
   return (
-    <div className="flex items-center rounded-2xl border border-sky-500 p-3">
-      <FaSearch className="searchBar-icon" />
+    <div className="flex items-center rounded-2xl border border-[#F9AA00] px-5 py-3 gap-5">
+      <FaSearch className="searchBar-icon text-2xl text-[#F9AA00]" />
       <input
-      className=""
+      className=" placeholder:text-black bg-transparent w-full focus:outline-none  focus:border-none focus:bg-transparent text-lg"
         type="text"
-        placeholder="Buscando Multimetro..."
+        placeholder="Buscar..."
         value={value}
         onChange={changeInput}
       />
