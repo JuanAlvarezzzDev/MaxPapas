87bad97bdc40abc6cd6c7bf924d9af8e4bf5ab1d
JuanAlvarezzzDev <juanalvarezzz.dev@gmail.com>
2024-03-19 22:43:47 -0500

    Header Sticky

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -7,20 +7,25 @@ export const Header = () => {
   const { busqueda, handleChangeBusqueda, showSidebar, setShowSidebar } =
     useQuisco();
   return (
-    <div className="bg-[#f9aa0022] px-3 py-5 w-full flex justify-around items-center gap-5">
-      <div>
+    <div className="bg-white py-2 px-5 w-full flex items-center justify-between gap-5 sticky top-0 left-0 z-40 shadow-md">
+
+      <div className="w-20"><img  className="w-full " src="/img/logo.svg" alt="Logo Maxpapas"/></div>
+      
+      <div className="w-3/4 md:w-2/4 ">
         <SearchBar
-          className="w-1/2"
+          className="w-full"
           value={busqueda}
           changeInput={handleChangeBusqueda}
         />
       </div>
+
       <div className="bg-[#F9AA00] p-1 rounded-lg">
         <LuMenuSquare
           onClick={() => setShowSidebar(!showSidebar)}
           className="text-4xl text-white"
         />
       </div>
+
     </div>
   );
 };
