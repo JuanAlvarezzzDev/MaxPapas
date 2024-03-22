978f6c6582112a8fa7295a3dc9457f7043d89acd
JuanAlvarezzzDev <juanalvarezzz.dev@gmail.com>
2024-03-21 23:29:23 -0500

    create function obtenerProductoId

--- client/src/store/useProduct.jsx
+++ client/src/store/useProduct.jsx
@@ -3,15 +3,27 @@ import { useEffect, useState } from "react";
 const useProduct = (api) => {
   const [producto, setProducto] = useState({});
   const [listProductos, setListProductos] = useState([]);
-
+  
   const obtenerProductos = async () => {
     try {
       const data = await api.fetch("productos");
-      setListProductos(data);
+      // console.log(data.data)
+      setListProductos(data.data);
     } catch (error) {
       console.error(error);
     }
   };
+
+  const obtenerProductoId = async (id)=>{
+    try{
+      const data = await api.fetch(`productos/${id}`);
+      // console.log(data)
+      return data
+      
+    } catch (error){
+      console.log(error)
+    }
+  }
 
   const handleClickProductoAgotado = async (id) => {
     try {
@@ -28,7 +40,7 @@ const useProduct = (api) => {
   useEffect(() => {
     obtenerProductos();
   }, []);
-  return {producto, setProducto, listProductos, obtenerProductos, handleClickProductoAgotado, handleSetProducto};
+  return {producto, setProducto, listProductos, obtenerProductos, handleClickProductoAgotado, handleSetProducto, obtenerProductoId};
 };
 
 export default useProduct;
