e2fd94499f16dc874eed836f88c992edeb05817e
JuanAlvarezzzDev <juanalvarezzz.dev@gmail.com>
2024-03-21 23:31:57 -0500

    Styles of Sidebar

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -2,16 +2,16 @@ import useQuisco from "../hooks/useQuiosco";
 import Categoria from "./Categoria";
 import { useAuth } from "../hooks/useAuth";
 
-export default function Sidebar() {
+export const SideBar = () => {
   const { categorias, showSidebar, setShowSidebar } = useQuisco();
   const { logout, user } = useAuth({ middleware: "auth" });
 
   return (
     <aside
-      className={`top-0  left-0 w-full h-screen flex z-50 ${showSidebar ? "fixed" : "hidden"} `}
+      className={`top-0  left-0 w-screen h-screen flex z-50 ${showSidebar ? "fixed" : "hidden"} `}
     >
       <div
-        className={`flex w-7/12 h-full justify-around flex-col items-center bg-white py-5`}
+        className={`flex h-full w-[500px] md:w-1/3 justify-around flex-col items-center bg-white py-5`}
       >
         <p className=" text-xl text-center font-bold w-full ">
           ¡Max {user?.name}!
@@ -35,7 +35,7 @@ export default function Sidebar() {
       </div>
 
       <div
-        className="w-5/12 h-full bg-black opacity-[0.50]"
+        className=" w-screen h-full bg-black opacity-[0.50]"
         onClick={() => setShowSidebar(false)}
       ></div>
     </aside>
