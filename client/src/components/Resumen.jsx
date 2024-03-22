0c6502a11dcfcffd39f1538a1c17591bca23fed8
JuanAlvarezzzDev <juanalvarezzz.dev@gmail.com>
2024-03-21 23:32:33 -0500

    comprobarPedido position absolute

--- client/src/components/Resumen.jsx
+++ client/src/components/Resumen.jsx
@@ -26,7 +26,11 @@ export default function Resumen() {
 
   return (
     <>
-      <aside className={`${asideResumen ? "fixed" : "hidden"} z-40 md:w-72 w-full bg-white md:h-screen h-full overflow-y-auto bottom-0 left-0 p-5 md:relative md:block`}>
+      <aside
+        className={`${
+          asideResumen ? "fixed" : "hidden"
+        } z-40 md:w-72 w-full bg-white md:h-screen h-full overflow-y-auto bottom-0 left-0 p-5 md:relative md:block`}
+      >
         <h1 className="text-4xl font-black">Mi Pedido</h1>
         <p className="text-lg my-5">
           Aquí podrás ver el resumen y totales de tu pedido
@@ -44,24 +48,27 @@ export default function Resumen() {
           )}
         </div>
 
-        <p className="text-xl mt-10 hidden md:block">
-          Total: {""}
-          {formatearDinero(total)}
-        </p>
-
-        <form className="w-full relative" onSubmit={handleSubmit}>
-          <div className="mt-5 ">
-            <input
-              type="submit"
-              className={`${
-                comprobarPedido() || pedidoEnviado ? "bg-stone-400" : "bg-black"
-              } 
+        <div className="absolute bottom-0 right-0 p-5 w-full">
+          <p className="text-xl mt-10 hidden md:block">
+            Total: {""}
+            {formatearDinero(total)}
+          </p>
+          <form className="w-full relative" onSubmit={handleSubmit}>
+            <div className="mt-3 ">
+              <input
+                type="submit"
+                className={`${
+                  comprobarPedido() || pedidoEnviado
+                    ? "bg-stone-400"
+                    : "bg-black"
+                } 
                             px-5 py-2 rounded uppercase font-bold text-white text-center w-full cursor-pointer`}
-              value={pedidoEnviado ? "Pedido Enviado" : "Confirmar Pedido"}
-              disabled={comprobarPedido() || pedidoEnviado}
-            />
-          </div>
-        </form>
+                value={pedidoEnviado ? "Pedido Enviado" : "Confirmar Pedido"}
+                disabled={comprobarPedido() || pedidoEnviado}
+              />
+            </div>
+          </form>
+        </div>
       </aside>
 
       <div className="fixed md:hidden bottom-0 left-0 w-full h-[60px] bg-white flex justify-around items-center z-20">
